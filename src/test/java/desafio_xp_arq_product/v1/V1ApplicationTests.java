38a5e51950c5d1f338b294faf9977e3477a001c6
felipematheus1337 <lipehbr@gmail.com>
2026-09-14 17:27:10 -0300

    Aplicação criada, Docker para banco de dados PostgreSQL e app.yml configurado.

---
+++ src/test/java/desafio_xp_arq_product/v1/V1ApplicationTests.java
@@ -0,0 +1,13 @@
+package desafio_xp_arq_product.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
